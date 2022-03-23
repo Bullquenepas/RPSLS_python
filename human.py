4cc462d7eee47a87ceed6b6f54180bdccd398e0c
Alex <thatsoalex@gmail.com>
2022-03-23 17:03:48 -0500

    defined human and ai class and started work on humans choose gesture

---
+++ human.py
@@ -0,0 +1,14 @@
+from player import Player
+
+class Human(Player):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def choose_gesture(self):
+        self.chosen_gesture = self.gesture[0:4]
+        user_input = int(input('Pick a gesture'))
+        print(self.choose_gesture)
+    
+    
+    
+    
