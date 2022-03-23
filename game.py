1277c663ef2d6a83190acef29206e9059c78e48e
Christopher Bonilla <Bullquenepas>
2022-03-23 17:08:24 -0400

    Added new class files. Modified game.py. Modified play.py.

---
+++ game.py
@@ -0,0 +1,13 @@
+#Display welcome message // display rules of game
+class Game:
+    def __init__(self):
+        pass
+
+    def welcome():
+        print('Welcome to a wonderful game of chance!')
+        print('Each player picks a variable and reveals it at the same time. The winner is the one who defeats the others. In a tie, the process is repeated until a winner is found.')
+    # greeting_message = welcome()
+
+    def display_gesture_options(self): 
+        print(self.gesture)
+    display_gesture = display_gesture_options()
---
+++ player.py
@@ -0,0 +1,8 @@
+
+
+
+class Player:
+    def __init__(self, name):
+        self.gesture = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        self.player = name
+    
